cfe6303169ff2d0c6e3643f0d679035c47a4672c
DHARINEESH N <dharineesh812@gmail.com>
2026-06-23 10:13:00 +0530

    Add files via upload

---
+++ LeetCode200.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> vis;
+    void dfs (vector<vector<char>>& grid , int i ,int j){
+        vis[i][j] = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        for(int k = 0;k < 4;k++){
+            int nr = i + dr[k];
+            int nc = j + dc[k];
+            if(nr >=0 && nr < n && nc >=0 && nc < m && grid[nr][nc]=='1' && ! vis [nr][nc])
+                dfs(grid,nr,nc);
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        vis.resize(n,vector<int>(m));
+        for(int i = 0 ;i < n ;i++){
+            for(int j = 0 ;j < m ;j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    cnt++;
+                    dfs(grid , i , j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
