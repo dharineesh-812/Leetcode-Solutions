a3266b76d0facb98aee9e6f0c710f099195b90ac
dharineesh-812 <dharineesh812@gmail.com>
2026-07-06 13:35:50 +0530

    Sum of all subarray

---
+++ contribution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);    
+    for(int i = 0;i < n;i++)
+        cin>>nums[i];
+    int sum = 0;
+    for(int i = 0;i < n;i++)
+        sum+= nums[i] * (i+1) * (n - i);
+    cout << sum;
+ }
