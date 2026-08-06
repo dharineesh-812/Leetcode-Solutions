0a7fb5004c8f4d1cbfb5515ff16d4205223f16f4
DHARINEESH N <dharineesh812@gmail.com>
2026-08-06 09:55:21 +0530

    Time: 12 ms (41.45%), Space: 31 MB (65.76%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int dp[303][303];
-    int rec(vector<vector<char>>& mat , int i , int j){
-        if(i < 0 || i >= mat.size() || j < 0 || j >= mat[0].size())
-            return 0;
-        if(mat[i][j] == '0')
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = 1 + min({rec(mat , i + 1 , j) , rec(mat , i , j + 1) , rec(mat , i + 1 , j + 1)});
-    }
-    int maximalSquare(vector<vector<char>>& matrix) {
-        memset(dp , -1 , sizeof(dp));
+    int maximalSquare(vector<vector<char>>& mat) {
+        int m = mat.size() , n = mat[0].size();
+        vector<vector<int>>dp(m, vector<int>(n , 0));
         int ans = 0;
-        int n = matrix.size() , m = matrix[0].size();
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
-                ans = max(ans , rec(matrix , i , j));
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(mat[i][j] == '0')
+                    dp[i][j] = 0;
+                else{
+                    if(i - 1 >=0 && j -1 >= 0)
+                        dp[i][j] = 1 + min({dp[i - 1][j],
+                                            dp[i][j-1], 
+                                            dp[i - 1][j - 1]});
+                    else
+                        dp[i][j] = 1;
+                }
+                ans = max(ans , dp[i][j]);
             }
         }
         return ans * ans;
