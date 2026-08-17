19598fdb20bae0f48ea8dd0c390a7afa7f71eb5d
DHARINEESH N <dharineesh812@gmail.com>
2026-08-17 10:19:43 +0530

    Time: 18 ms (97.96%), Space: 16.2 MB (90.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,25 +2,24 @@
 #define vvi vector<vi>
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid , int i , int j , vvi &vis){
-        vis[i][j] = 1;
+    void dfs(vector<vector<char>>& grid , int i , int j ){
+        grid[i][j] = '0';
         int dx[4] = {-1 , 0 , 1 , 0};
         int dy[4] = {0 , 1 , 0 , -1};
         for(int k = 0; k < 4;k++){
             int nx = i + dx[k];
             int ny = j + dy[k];
-            if(nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[i][j] == '1' && !vis[nx][ny])
-                dfs(grid , nx , ny , vis);
+            if(nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[nx][ny] == '1')
+                dfs(grid , nx , ny);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size() , n = grid[0].size() , cnt = 0;
-        vvi vis(m,vi(n,0));
         for(int i= 0;i < m;i++){
             for(int j = 0;j < n;j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
+                if(grid[i][j] == '1'){
                     cnt++;
-                    dfs(grid , i , j , vis);
+                    dfs(grid , i , j);
                 }                    
             }
         }
