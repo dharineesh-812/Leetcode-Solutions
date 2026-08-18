4a19da177888428431811052e06a44269454e4bf
DHARINEESH N <dharineesh812@gmail.com>
2026-08-18 10:33:27 +0530

    Time: 3 ms (62.13%), Space: 17.8 MB (65.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,40 @@
 #define vi vector<int>
-#define pi pair<int,int>
+#define pi pair<int, int>
 #define F first
 #define S second
 const int inf = 1e9;
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,int k) {
         vector<vector<pi>> adj(n);
-        for(int i = 0; i < flights.size();i++){
-            int u = flights[i][0] , v = flights[i][1] , c = flights[i][2];
-            adj[u].push_back({v , c});
+        int res = inf;
+        for (int i = 0; i < flights.size(); i++) {
+            int u = flights[i][0], v = flights[i][1], c = flights[i][2];
+            adj[u].push_back({v, c});
         }
-        using state = pi;
-        queue<state> q;
-        vi dist(n , inf);
+        using state = pair<int, pi>;
+        set<state> st;
+        vi dist(n, inf);
         dist[src] = 0;
-        q.push({0 , src});
-        int used = 0;
-        while(!q.empty() && used <= k){
-            int si = q.size();
-            while(si--){
-                state it = q.front();
-                q.pop();
-                int c = it.F , u = it.S;
-                for(auto [v , d] : adj[u]){
-                    int nc = d + c;
-                    if(nc <= dist[v]){
-                        dist[v] = nc;
-                        q.push({nc , v});
-                    }
-                }
+        st.insert({0, {0, src}});
+        while (!st.empty()) {
+            state it = *st.begin();
+            st.erase(st.begin());
+            int used = it.F, c = it.S.F, u = it.S.S;
+            for (auto [v, d] : adj[u]) {
+                int nc = d + c;
+                if(v == dst)
+                    res = min(res , nc);
+                if (used >= k) 
+                    continue;
+                if(nc < dist[v]) {
+                    dist[v] = nc;
+                    st.insert({used + 1 ,{nc, v}});
+                } 
             }
-            used++;
         }
-        if(dist[dst] != inf)
-            return dist[dst];
+        if (res != inf)
+            return res;
         return -1;
     }
 };
