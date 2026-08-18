8ae3091af49e7e5860452cdd5713357a49749d30
DHARINEESH N <dharineesh812@gmail.com>
2026-08-18 10:02:18 +0530

    Time: 0 ms (100%), Space: 17.8 MB (73.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,35 @@
 #define vi vector<int>
-#define vipi vector<vector<pair<int,int>>>
+#define pi pair<int,int>
+#define F first
+#define S second
 const int inf = 1e9;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vipi adj(n);
-        for(int i = 0;i < flights.size();i++){
+        vector<vector<pi>> adj(n);
+        for(int i = 0; i < flights.size();i++){
             int u = flights[i][0] , v = flights[i][1] , c = flights[i][2];
             adj[u].push_back({v , c});
         }
-        vi dist(n, inf);
-        dist[src] = 0;
-        using state = pair<int,int>;
+        using state = pi;
         queue<state> q;
-        q.push({0 ,src});
+        vi dist(n , inf);
+        dist[src] = 0;
+        q.push({0 , src});
         int used = 0;
         while(!q.empty() && used <= k){
-            int size = q.size();
-            while(size--){
-
-             auto[d,u]= q.front();
-            q.pop();
-             
-            for(auto[v,c]: adj[u]){
-                 int nc = d + c;
-                if(nc < dist[v]){
-                    dist[v] = nc;
-                    q.push({nc , v});
-                }    
-            }
+            int si = q.size();
+            while(si--){
+                state it = q.front();
+                q.pop();
+                int c = it.F , u = it.S;
+                for(auto [v , d] : adj[u]){
+                    int nc = d + c;
+                    if(nc <= dist[v]){
+                        dist[v] = nc;
+                        q.push({nc , v});
+                    }
+                }
             }
             used++;
         }
