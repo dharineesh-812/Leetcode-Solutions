67b499127fc1bfcc1c3b5c2012cd4b9fe22e71e6
DHARINEESH N <dharineesh812@gmail.com>
2026-08-18 15:00:16 +0530

    Time: 1 ms (83.57%), Space: 19.3 MB (68.33%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,32 @@
+#define vi vector<int>
+#define vvi vector<vi>
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vi indegree(numCourses);
+        vvi adj(numCourses);
+        for(int i = 0; i < prerequisites.size();i++){
+            int u = prerequisites[i][0] , v = prerequisites[i][1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int>q;
+        for(int i = 0; i < numCourses;i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        int cnt = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            cnt += 1;
+            for(int nei : adj[node]){
+                indegree[nei]--;
+                if(indegree[nei] == 0){
+                    q.push(nei);
+                }
+            }
+        }
+        return cnt == numCourses;
+    }
+};
