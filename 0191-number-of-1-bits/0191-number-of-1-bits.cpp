87ef6ca09b2d5ab603ecf1deb162c9c99dfab875
DHARINEESH N <dharineesh812@gmail.com>
2026-08-28 09:30:29 +0530

    Time: 0 ms (100%), Space: 8.2 MB (82.4%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(int n) {
         int cnt = 0;
         while(n){
-            cnt += n & 1;
-            n = n >> 1;
+            n = n & (n - 1);
+            cnt += 1;
         }
         return cnt;
     }
